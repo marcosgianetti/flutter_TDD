e0519b00d4541fad8ace42779f78c3e0084d5d3b
Marcos Gianetti <marcos.c.gianetti@hotmail.com>
2022-02-11 16:27:30 -0300

    ensure RemoteAuthentication calss HttpClient with correct body

--- flutter_tdd/test/data/usecases/remote_authenticartion_test.dart
+++ flutter_tdd/test/data/usecases/remote_authenticartion_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tdd/domain/usecases/usecases.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -11,30 +12,52 @@ class RemoteAuthetication {
     @required this.httpClient,
     @required this.url,
   });
-  Future<void> auth() async {
-    await httpClient.request(url: url);
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {
+      'email': params.email,
+      'password': params.secret,
+    };
+
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
 abstract class HttpClient {
   Future<void> request({
     @required String url,
+    @required String method,
+    Map body,
   });
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test('Should call http client with correct URL', () async {
-    //Arrange
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthetication(httpClient: httpClient, url: url);
+  RemoteAuthetication sut;
+  HttpClientSpy httpClient;
+  String url;
 
+  setUp(() {
+    //Arrange
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthetication(httpClient: httpClient, url: url);
+  });
+  test('Should call http client with correct values', () async {
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
     //Action
-    await sut.auth();
+    await sut.auth(params);
 
     //Accert
-    verify(httpClient.request(url: url));
+    verify(
+      httpClient.request(
+        url: url,
+        method: 'post',
+        body: {
+          'email': params.email,
+          'password': params.secret,
+        },
+      ),
+    );
   });
 }
