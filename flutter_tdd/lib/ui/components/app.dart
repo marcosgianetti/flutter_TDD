0b651db90e0f9c93e8384874adeb857a9ef9f71e
Marcos Gianetti <marcos.c.gianetti@hotmail.com>
2022-03-22 10:15:49 -0300

    Add app icon

--- flutter_tdd/lib/ui/components/app.dart
+++ flutter_tdd/lib/ui/components/app.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_tdd/ui/pages/pages.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     final primaryColor = Color.fromRGBO(136, 14, 79, 1);
     final primaryColorDark = Color.fromRGBO(96, 0, 39, 1);
     final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
@@ -37,16 +40,16 @@ class App extends StatelessWidget {
           alignLabelWithHint: true,
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-
-                //color: ColorScheme.light(primary: primaryColor),
-                primary: primaryColor,
-                onSurface: primaryColorLight,
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                //     textStyle:Eleva ,// ButtonTextTheme.primary,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ))),
+          style: ElevatedButton.styleFrom(
+            primary: primaryColor,
+            onSurface: primaryColorLight,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            //     textStyle:Eleva ,// ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+        ),
         buttonTheme: ButtonThemeData(
           colorScheme: ColorScheme.light(primary: primaryColor),
           buttonColor: primaryColor,
